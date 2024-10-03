80411439d39f04fb6c0b6ccc2e8a4541c147ad2a
皮喆 <pizhecode@outlook.com>
2024-10-04 02:08:58 +0800

    积分消费记录

--- pages/my/mypurse/mypurse.vue
+++ pages/my/mypurse/mypurse.vue
@@ -1,14 +1,38 @@
 <template>
 	<view>
-		我的钱包
+       <view class=" tac mt40">
+         <view class="fs32">积分余额<text>12</text></view>
+        <view class="mt60 fs30">
+            <view class="df aic jcc">
+                <view class="mr50">累计收入:<text>12</text></view>
+                <view class="ml50">累计支出:<text>12</text></view>
+            </view>
+        </view>
+       </view>
+       
+       <fui-tabs :tabs="tabs" @change="Select"></fui-tabs>
+       
+       
+       <view class="m30">
+          <view class="df aic jcsb">
+             <view>捐赠兑换</view>
+             <view class="cf00">+56.00</view>
+          </view>
+          <view class="df aic jcsb">
+             <view>捐赠编号<text>132312312</text></view>
+             <view>2424123123</view>
+          </view>
+       </view>
 	</view>
 </template>
 
 <script>
+  import fuiTabs from "@/components/firstui/fui-tabs.vue"
 	export default {
+    components:{fuiTabs},
 		data() {
 			return {
-				
+				tabs: ['全部','收入','支出'],
 			}
 		},
 		onShow() {
@@ -18,7 +42,9 @@
 			
 		},
 		methods: {
-			
+			Select(value){
+			  console.log(value);
+			},
 		}
 	}
 </script>
--- pages/my/mypurse/trading.vue
+++ pages/my/mypurse/trading.vue
@@ -1,6 +1,17 @@
 <template>
 	<view>
-		消费明细
+     <uni-section title="日期区间筛选" type="line">
+        <view class="m30">
+           <view class="df aic jcsb">
+              <view>订单编号：<text>132312312</text></view>
+              <view class="cf00">+56.00</view>
+           </view>
+           <view class="df aic jcsb">
+              <view>时间xxx</view>
+              <view>微信支付</view>
+           </view>
+        </view>
+     </uni-section>
 	</view>
 </template>
 
